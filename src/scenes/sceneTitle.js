5866da69898e4c755846b0f4dfa15851452527fd
bonder-axd <bonder@academixdirect.com>
2020-04-19 20:10:19 -0700

    New title!

--- src/scenes/sceneTitle.js
+++ src/scenes/sceneTitle.js
@@ -32,7 +32,8 @@ export class SceneTitle extends BaseScene {
         //
         //
       //  this.placeImage('title', 27, .8);
-        this.placeText("Game Title",27,"TITLE_TEXT");
+
+        this.placeImage("puppy_peril", 38, .5, false);
         //
         //
         //
